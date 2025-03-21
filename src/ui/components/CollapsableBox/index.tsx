9e0541fdad050cacf4fbae453ee7931f6152d026
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-03-21 18:43:48 -0300

    refactor(component): ♻️  create CollapsableBox

--- src/ui/components/CollapsableBox/index.tsx
+++ src/ui/components/CollapsableBox/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+import cx from 'classnames'
 
 interface CollapsableBoxProps {
   children: ReactNode
@@ -12,11 +13,22 @@ interface CollapsableBoxProps {
 function CollapsableBox({ open, label, children, toggleBox }: CollapsableBoxProps) {
   return (
     <Disclosure defaultOpen={open}>
-      <DisclosureButton className="flex items-center gap-2 w-full" onClick={toggleBox}>
+      <DisclosureButton
+        className={cx(
+          'flex items-center gap-2 w-full p-4 bg-brand-gray-300 hover:bg-brand-gray-500',
+          {
+            'rounded-t-lg': open,
+            'rounded-lg': !open
+          }
+        )}
+        onClick={toggleBox}
+      >
         {label}
         {/* <ChevronDownIcon className={cx("w-5", { "rotate-180": open })} /> */}
       </DisclosureButton>
-      <DisclosurePanel className="w-full">{children}</DisclosurePanel>
+      <DisclosurePanel className="w-full border border-brand-gray-300 p-4 rounded-b-lg">
+        {children}
+      </DisclosurePanel>
     </Disclosure>
   )
 }
