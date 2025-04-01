09e9db88a83e1e27f9f5c9b6941159c7bc8ceaeb
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-04-01 17:01:00 -0300

    refactor(request): ♻️  authentication use internal route to send request

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server'
+
+import type { SuccessResponse } from '@/types/apiPatterns'
+import type { UserAuth } from '@/types/apiResponses'
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  if (!body.password || !body.email) {
+    return NextResponse.json({ error: 'E-mail ou senha estão ausentes' }, { status: 400 })
+  }
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+  if (!baseUrl) return undefined
+
+  const loginUrl = `${baseUrl}/users/login`
+  const credentials = { email: body.email, password: body.password }
+
+  try {
+    const auth = await fetch(loginUrl, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    }).then((res) => res.json())
+
+    if (auth.error) {
+      throw Error('Não foi possivel completar atualizar suas credenciais')
+    }
+
+    const { data } = auth as SuccessResponse<UserAuth>
+
+    return NextResponse.json(
+      { message: 'Serviço chamado com sucesso', data: data },
+      { status: 200 }
+    )
+  } catch (err) {
+    return NextResponse.json({ error: 'Erro interno do servidor', details: err }, { status: 500 })
+  }
+}
