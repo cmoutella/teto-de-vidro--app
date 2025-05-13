d0e6fd1e2581c0bc223aa2b4197bb245aadf975c
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-05-12 22:10:01 -0300

    feat(ui): ✨  add comment types

---
+++ src/types/comment.ts
@@ -0,0 +1,25 @@
+import type { PropertyHuntingStage } from './targetProperty'
+
+export type AuthorPrivacy = 'allowed' | 'denied'
+
+export type CommentRelatedTo = 'lot' | 'property'
+
+export type CommentTopic = CommentRelatedTo | 'surroundings' | 'owner' | 'agency' | string
+
+export interface InterfaceComment {
+  id: string
+  comment: string
+  topic: CommentTopic
+
+  author: string
+  authorPrivacy: AuthorPrivacy
+
+  target?: {
+    targetId: string
+    stage: PropertyHuntingStage
+  }
+
+  updatedAt: string
+}
+
+export type TargetOnUpdateComment = Pick<InterfaceComment, 'comment' | 'topic' | 'author'>
