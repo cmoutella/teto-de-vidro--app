3d95aabb526bf74233db4eb046d000588670c882
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-10-11 09:16:23 -0300

    feat(welcome): personalData form

--- src/ui/forms/Welcome/PersonalData/index.tsx
+++ src/ui/forms/Welcome/PersonalData/index.tsx
@@ -1,39 +1,42 @@
 'use client'
-import type { ChangeEvent } from 'react'
 
-import Input from '@ui/base/form/inputs/Input'
 import type { FormSizes, FormTheme } from '@ui/base/shared/formTheme'
+
+import InputCPF from '@/ui/components/base/form/inputs/InputCPF'
+import InputFullDate from '@/ui/components/base/form/inputs/InputFullDate'
 
 interface PersonalDataWelcomeFormProps {
   values: { birthDate: string; cpf: string }
-  handleChange: (_e: ChangeEvent) => void
+  handleCPFChange: (_d: string) => void
+  handleDateChange: (_d: string) => void
 }
 
 const formThemeSize: FormSizes = 'lg'
 const themePallete: FormTheme = 'light'
 
-const PersonalDataWelcomeForm = ({ handleChange, values }: PersonalDataWelcomeFormProps) => {
+const PersonalDataWelcomeForm = ({
+  handleCPFChange,
+  handleDateChange,
+  values
+}: PersonalDataWelcomeFormProps) => {
   return (
     <form className="w-full flex flex-col gap-2 md:gap-3">
-      <Input
+      <InputFullDate
         label="Data de nascimento"
         labelStyle="text-white md:text-brand-primary-700"
-        name="birthDate"
         themeSize={formThemeSize}
         theme={themePallete}
-        placeholder={`01/01/1990`}
         value={values.birthDate}
-        onChange={handleChange}
+        onChange={handleDateChange}
       />
-      <Input
+      <InputCPF
         label="CPF"
         labelStyle="text-white md:text-brand-primary-700"
-        name="cpf"
         themeSize={formThemeSize}
         theme={themePallete}
         placeholder={`123.456.789-00`}
         value={values.cpf}
-        onChange={handleChange}
+        onChange={handleCPFChange}
       />
     </form>
   )
