eb492d366cc1f73d08b3950b8a183241b66af93a
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-10-28 14:58:27 -0300

    fix(navbar): nav position

--- src/ui/template/Navbar/PrivateNavbar.tsx
+++ src/ui/template/Navbar/PrivateNavbar.tsx
@@ -6,7 +6,7 @@ import { NavLogo } from './Logo'
 
 const Private = ({ user }: { user?: Omit<InterfaceUser, 'password'> }) => {
   return (
-    <div className="w-full py-2 sm:py-3 md:py-4 px-2 md:px-4 lg:px-14 flex justify-between items-center bg-brand-primary-700">
+    <div className="w-full py-2 sm:py-3 md:py-4 px-2 md:px-4 lg:px-14 flex justify-center items-center bg-brand-primary-700">
       <div className="container flex flex-row gap-6 justify-between">
         <NavLogo />
         <span className="flex items-center sm:hidden -translate-x-3">
--- src/ui/template/Navbar/PublicNavbar.tsx
+++ src/ui/template/Navbar/PublicNavbar.tsx
@@ -6,7 +6,7 @@ import { NavLogo } from './Logo'
 
 const PublicNavbar = ({ user }: { user?: Omit<InterfaceUser, 'password'> }) => {
   return (
-    <div className="w-full py-2 sm:py-3 md:py-4 px-2 md:px-4 lg:px-14 flex justify-between items-center bg-brand-primary-400">
+    <div className="w-full py-2 sm:py-3 md:py-4 px-2 md:px-4 lg:px-14 flex justify-center items-center bg-brand-primary-400">
       <div className="container flex flex-row gap-6 justify-between items-center">
         <NavLogo />
         <span className="flex items-center sm:hidden -translate-x-3">
